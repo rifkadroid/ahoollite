657474572e2983b152428686e45900f048a1bb4f
KONTROL Tecnologia <guzzy@uol.com.br>
2023-07-22 19:23:37 -0300

    Update builder_common.sh

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -271,9 +271,9 @@ make_world() {
 		|| print_error_pfS
 echo ">>>Starting cross compiling - kontrol commented out"
 	# Use the builder cross compiler from obj to produce the final binary.
-	BUILD_CC="${MAKEOBJDIRPREFIX}${FREEBSD_SRC_DIR}/${TARGET}.${TARGET_ARCH}/tmp/usr/bin/cc"
+	#BUILD_CC="${MAKEOBJDIRPREFIX}${FREEBSD_SRC_DIR}/${TARGET}.${TARGET_ARCH}/tmp/usr/bin/cc"
 
-	[ -f "${BUILD_CC}" ] || print_error_pfS
+	#[ -f "${BUILD_CC}" ] || print_error_pfS
 echo ">>>Starting crypto tools - kontrol commented out"
 	# XXX It must go to the scripts
 	[ -d "${STAGE_CHROOT_DIR}/usr/local/bin" ] \
