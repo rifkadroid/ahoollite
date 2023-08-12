2518bd66e84dfdcccf6fb0a608053efcd188e127
KONTROL Tecnologia <guzzy@uol.com.br>
2023-08-12 01:39:57 -0300

    Update build_freebsd.sh

--- build/scripts/build_freebsd.sh
+++ build/scripts/build_freebsd.sh
@@ -111,7 +111,7 @@ j="-j${njobs}"
 if [ -z "${skip_kernel}" ]; then
 	for kernel in ${KERNCONF:-pfSense}; do
 		run "Building kernel (${kernel})" \
-			"CFLAGS+=-Wno-bitfield-constant-conversion make -C ${srcdir} -s ${j} KERNCONF=${kernel}  buildkernel"
+			"make -C ${srcdir} -s ${j} KERNCONF=${kernel} WERROR=-Wno-bitfield-constant-conversion buildkernel"
 	done
 fi
 
